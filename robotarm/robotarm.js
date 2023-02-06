555ce4a782528208eb719d4b4edc25c2d47773d4
postie89 <t.mannion89@gmail.com>
2023-02-06 17:36:31 +0000

    altered data set

--- robotarm/robotarm.js
+++ robotarm/robotarm.js
@@ -4,10 +4,10 @@ Input: A set S of n points in the plane.
 Output: What is the shortest cycle tour that visits each point in the set S?
 */
 const points = [
-  [0, 2, 2, 3],
+  [0, 2, 3, 3],
   [2, 0, 4, 5],
   [3, 4, 0, 6],
-  [4, 5, 6, 0],
+  [3, 5, 6, 0],
 ];
 
 function robotTour(points) {}
